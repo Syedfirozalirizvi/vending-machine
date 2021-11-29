d0abfb8c8e5429274635fcd4109eed3afc3f6b69
Raghuveer1998 <raghuveer.thoughtwin@gmail.com>
2021-11-29 15:13:48 +0530

    final code

--- Blackjack.py
+++ Blackjack.py
@@ -1,68 +1,136 @@
+
 import random
 import os
 import time
 
 
+class blackjack:
+    def fun(self):
+
+        rows = 15
+        cols = 20
+        
+
+        player = 0
+        dealer = 0
+
+        for i in range(1,4):
+            time.sleep(2)
+            for k in range(rows):
+                print('|' +('-' if k in (0,rows-1) else ' ') * (cols-2) + '|')
+                
+            for l in range(rows):
+                print('|' +('-' if l in (0,rows-1) else ' ') * (cols-2) + '|')
+            
+                                
+            if i == 1:
+
+                for j in range(i):
+                    
+                    player_points = random.randint(2,11)   
+                    dealer_points = random.randint(2,11)
+                    
+
+                    print('Player Card :',player_points)
+                    print('Dealer Card :',dealer_points)
+                    print()
 
 
-player = 0
-dealer = 0
+                    player = player + player_points
+                    dealer = dealer + dealer_points
+
+                    print('Player Points:',player)
+                    print('Dealer Points :',dealer)
 
 
-for i in range(0,3):
+                    input('press enter to continue')
 
 
-    player_points = random.randint(2,11)   
-    dealer_points = random.randint(2,11)
-    time.sleep(2)
+                    
+            if i == 2:
 
-    print('Player Card Is :',player_points)
-    print('Dealer Card Is :',dealer_points)
+                    player_points = random.randint(2,11)   
+                    dealer_points = random.randint(2,11)
+                    
 
+                    print('Player Card :', player_points)
+                    print('Dealer Card : Hidden')
+                    print()
+                
 
-    player = player + player_points
-    dealer = dealer + dealer_points
+                    player = player + player_points
+                    dealer = dealer + dealer_points
 
-    print('Player Points:',player)
-    print('Dealer Points :',dealer)
-
-
-    input('press enter to continue')
-
-
-    if player > 21:
-        print('PLayer Is LOSE')
-        break
-
-    if dealer > 21:
-        print('Dealer Is LOSE')
-        break
+                    print('Player Points:',player)
+                    #print('Dealer Points :',dealer)
 
 
-
-    if player == 21:
-        print('Player is WIN')
-        break
-
-    if dealer == 21:
-        print('Dealer is WIN')
-        break    
+                    input('press enter to continue')
 
 
-print('#####game over######')
-print('Player Points Is : ',player)
-print('Dealer Points Is : ',dealer)
+                    
+            if i == 3:
+
+                    player_points = random.randint(2,11)   
+                    dealer_points = random.randint(2,11)
+                    
+
+                    print('Player Card :',player_points)
+                    print('Dealer Card :',dealer_points)
+                    print()
 
 
-if player == dealer:
-        print('A Tie Game')
+                    player = player + player_points
+                    dealer = dealer + dealer_points
+
+                    print('Player Points:',player)
+                    print('Dealer Points :',dealer)
 
 
-if player > dealer:
-        print('*********player is winer*********')
-else:
-        print('*********dealer is winer*********')
+                    input('press enter to continue')
 
+ 
+
+            if player > 21:
+                        print('PLayer Is LOSE')
+                        break
+
+            if dealer > 21:
+                        print('Dealer Is LOSE')
+                        break
+
+
+
+            if player == 21:
+                        print('Player is WIN')
+                        break
+
+            if dealer == 21:
+                        print('Dealer is WIN')
+                        break    
+
+
+
+
+        print('#####game over######')
+        print('Player Points Is : ',player)
+        print('Dealer Points Is : ',dealer)
+
+
+        if player == dealer:
+            print('A Tie Game')
+
+
+        if player > dealer:
+            print('*********player is winer*********')
+        else:
+            print('*********dealer is winer*********')
+
+
+
+
+obj = blackjack()
+obj.fun()
 
 
 
